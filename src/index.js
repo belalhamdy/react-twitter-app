3b2b5ebdd2b167bca3e914c1c8fae246323c42aa
Belal Hamdy <belalhezzat@gmail.com>
2020-04-08 02:46:44 +0200

    finalizing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App'
 import {createStore} from "redux";
 import {Provider} from 'react-redux'
 import reducer from "./Reducers";
-import middleWare from "./Midllewares"
+import middleWare from "./Middlewares"
 
 const store = createStore(reducer,middleWare);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
